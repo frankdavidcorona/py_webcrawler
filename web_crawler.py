c5115e01ec0805acc957ea43f7d5adaf590f3507
Frank Corona Prendes <frank.corona@pm.me>
2023-04-14 23:33:07 -0300

    refactor(web_crawler.py): rename get_links to get_page_content and add extract_links function feat(web_crawler.py): add web_crawler function to crawl web pages The get_links function has been renamed to get_page_content to better reflect its purpose. The extract_links function has been added to extract links from the page content. The web_crawler function has been added to crawl web pages starting from a given URL. It takes an optional max_pages parameter to limit the number of pages to crawl. The crawled pages are returned as a set.

--- web_crawler.py
+++ web_crawler.py
@@ -1,20 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-def get_links(url):
+def get_page_content(url):
     response = requests.get(url)
-    content = response.content
-    soup = BeautifulSoup(content, 'html.parser')
+    response.raise_for_status()
+    return response.text
+
+def extract_links(page_content, base_url):
+    soup = BeautifulSoup(page_content, "html.parser")
     links = []
-    for link in soup.find_all('a'):
-        href = link.get('href')
-        if href and not href.startswith('#'):
-            links.append(href)
+    for link in soup.find_all("a", href=True):
+        full_url = urljoin(base_url, link["href"])
+        links.append(full_url)
     return links
 
+def web_crawler(start_url, max_pages=10):
+    visited_pages = set()
+    pages_to_visit = [start_url]
+
+    while pages_to_visit and len(visited_pages) < max_pages:
+        current_url = pages_to_visit.pop(0)
+        if current_url not in visited_pages:
+            try:
+                print(f"Crawling: {current_url}")
+                page_content = get_page_content(current_url)
+                links = extract_links(page_content, current_url)
+                pages_to_visit.extend(links)
+                visited_pages.add(current_url)
+            except requests.HTTPError as e:
+                print(f"Failed to crawl {current_url}: {e}")
+
+    return visited_pages
+
 if __name__ == "__main__":
-    url = input("Enter a URL to crawl: ")
-    links = get_links(url)
-    print("Links:")
-    for link in links:
-        print(link)
+    start_url = "https://as.com"
+    crawled_pages = web_crawler(start_url)
+    print("Crawled pages:")
+    for page in crawled_pages:
+        print(page)
